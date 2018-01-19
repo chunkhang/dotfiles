d76ce000548c4ed1b32d0e68988ef01c0ec5ab24
Marcus Mu <chunkhang@gmail.com>
2018-01-19 10:32:35 +0800

    Update git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,11 +10,11 @@ alias deploy='git push heroku master'
 alias papertrail='heroku addons:open papertrail'
 
 # Git
-alias ga='f() { git add $1 };f'
-alias gaa='git add .'
+alias ga='f() { git add $@ };f'
 alias gc='git commit'
 alias gcm='f() { git commit -m $1 };f'
 alias gd='f() { git diff $1 | cdiff -s };f'
+alias gi='git init'
 alias gk='f() { git checkout $1 };f'
 alias gl='git log --oneline --decorate --color --graph --all'
 alias gm='f() { git merge $1 };f'
