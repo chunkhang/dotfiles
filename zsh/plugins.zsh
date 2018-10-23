4ca83fc3bf01e25d81a7a57ea27be81d690d02ed
Marcus Mu <chunkhang@gmail.com>
2018-10-23 16:40:42 +0800

    Lazy load rbenv and pyenv

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -6,17 +6,35 @@ source $ZSH/oh-my-zsh.sh
 source `brew --prefix`/etc/profile.d/z.sh
 
 # Rbenv
-eval "$(rbenv init -)"
+# https://github.com/kadaan/zsh-rbenv-lazy/blob/master/rbenv-lazy.plugin.zsh
+if type rbenv &> /dev/null; then
+  local RBENV_SHIMS="${RBENV_ROOT:-${HOME}/.rbenv}/shims"
+  export PATH="${RBENV_SHIMS}:${PATH}"
+  source $(which rbenv)/../../completions/rbenv.zsh
+  function rbenv() {
+    unset -f rbenv > /dev/null 2>&1
+    eval "$(command rbenv init -)"
+    rbenv "$@"
+  }
+fi
 
 # Pyenv
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+# https://github.com/davidparsson/zsh-pyenv-lazy/blob/master/pyenv-lazy.plugin.zsh
+export PYENV_ROOT="${PYENV_ROOT:=${HOME}/.pyenv}"
+if ! type pyenv > /dev/null && [ -f "${PYENV_ROOT}/bin/pyenv" ]; then
+    export PATH="${PYENV_ROOT}/bin:${PATH}"
+fi
+if type pyenv > /dev/null; then
+    export PATH="${PYENV_ROOT}/bin:${PYENV_ROOT}/shims:${PATH}"
+    function pyenv() {
+        unset -f pyenv
+        eval "$(command pyenv init -)"
+        pyenv $@
+    }
+fi
 
 # Zmv
 autoload zmv
-
-# TheFuck
-eval $(thefuck --alias)
 
 # Syntax highlighting
 source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
