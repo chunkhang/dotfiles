6de0c978202cf1d1415b5f53f19eb715dfaa37ea
Marcus Mu <chunkhang@gmail.com>
2019-07-18 17:58:25 +0800

    Add zoom url to browser script

--- scripts/weechat/browser.zsh
+++ scripts/weechat/browser.zsh
@@ -1,15 +1,21 @@
 #!/usr/bin/env zsh
 
 for url in "$@"; do
-    firefox="Firefox"
-    chrome="Google Chrome"
+  firefox="Firefox"
+  chrome="Google Chrome"
 
-    # Determine browser to use
-    browser="$firefox"
-    if [[ "$url" =~ "https://.*\.slack\.com/call/.*" ]]; then
-        browser="$chrome"
-    fi
+  # Determine browser to use
+  browser="$firefox"
 
-    # Open url
-    open -a "$browser" "$url"
+  if [[ "$url" =~ "https://.*\.slack\.com/call/.*" ]]; then
+    browser="$chrome"
+  fi
+
+  if [[ "$url" =~ "https://zoom\.us/.*" ]]; then
+    browser="$chrome"
+    url=$(echo "$url" | sed 's/\/j\//\/wc\//' | sed 's/?/\/join?/')
+  fi
+
+  # Open url
+  open -a "$browser" "$url"
 done
