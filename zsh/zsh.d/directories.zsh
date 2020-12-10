4eb19405821a4d50986aeb589072186bda752a6e
Marcus Mu <chunkhang@gmail.com>
2020-12-11 02:04:31 +0800

    Move ctrl p to directories.zsh

--- zsh/zsh.d/directories.zsh
+++ zsh/zsh.d/directories.zsh
@@ -1,3 +1,21 @@
 setopt AUTO_CD
 
+# Load zmv
 autoload -U zmv
+
+# Use ctrl-p for switching directory
+function _ctrl_p() {
+  local dir=$( \
+    z -l | tac | head -n 100 | awk '{print $2}'| \
+    grep --invert-match --line-regexp --fixed-strings "$(pwd)" | \
+    sed "s,${HOME},~," | \
+    fzf --reverse --tiebreak=end,index --exact --height 40% | \
+    sed "s,~,${HOME}," \
+  )
+  if [[ -d "$dir" ]]; then
+    cd "$dir"
+  fi
+  zle fzf-redraw-prompt
+}
+zle -N _ctrl_p
+bindkey '^P' _ctrl_p
