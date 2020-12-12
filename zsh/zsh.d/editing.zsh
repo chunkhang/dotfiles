3da1e75e0282c3f706d8814dccd3636f40b6b675
Marcus Mu <chunkhang@gmail.com>
2020-12-12 22:31:10 +0800

    Update editing.zsh

--- zsh/zsh.d/editing.zsh
+++ zsh/zsh.d/editing.zsh
@@ -1,14 +1,7 @@
 # Use emacs key bindings
-# bindkey -e
-bindkey '^A' beginning-of-line
-bindkey '^E' end-of-line
-bindkey '^?' backward-delete-char
-bindkey '^[b' vi-backward-blank-word
-bindkey '^[f' vi-forward-blank-word
+bindkey -e
 
-# TODO: alt-delete
-# /etc/zshrc
-
-# autoload -U edit-command-line
-# zle -N edit-command-line
-# bindkey -M vicmd v edit-command-line
+# Control space to edit command in editor
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey "^@" edit-command-line
