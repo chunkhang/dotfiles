ad75694c1c59e0f3b33135b0877e20ed9cb1de0e
Marcus Mu <chunkhang@gmail.com>
2020-12-14 09:10:19 +0800

    Fix editing.zsh

--- zsh/zsh.d/editing.zsh
+++ zsh/zsh.d/editing.zsh
@@ -1,5 +1,10 @@
-# Use emacs key bindings
-bindkey -e
+bindkey '^a' beginning-of-line
+bindkey '^e' end-of-line
+
+bindkey '^[b' vi-backward-blank-word
+bindkey '^[f' vi-forward-blank-word
+
+bindkey '^[^?' vi-backward-kill-word
 
 # Control space to edit command in editor
 autoload -U edit-command-line
