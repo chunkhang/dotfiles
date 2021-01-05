172c9b8482d2168cf82a838d2fe346fba1f2a580
Marcus Mu <chunkhang@gmail.com>
2021-01-05 23:49:50 +0800

    Setup alacritty

--- zsh/zsh.d/editing.zsh
+++ zsh/zsh.d/editing.zsh
@@ -1,10 +1,8 @@
-bindkey '^a' beginning-of-line
-bindkey '^e' end-of-line
-
 bindkey '^[b' vi-backward-blank-word
 bindkey '^[f' vi-forward-blank-word
 
-bindkey '^[^?' vi-backward-kill-word
+bindkey '^[a' beginning-of-line
+bindkey '^[e' end-of-line
 
 # Control space to edit command in editor
 autoload -U edit-command-line
