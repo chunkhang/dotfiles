f5903996ddae12807db12981d6aed24b30115649
Marcus Mu <chunkhang@gmail.com>
2021-04-29 17:03:03 +0800

    Fix git prompt

--- zsh/zsh.d/prompt.zsh
+++ zsh/zsh.d/prompt.zsh
@@ -15,7 +15,7 @@ function _prompt() {
   local git_dir=$(_git rev-parse --git-dir 2>/dev/null | xargs realpath)
   if [[ -n "$git_dir" && ! "$PWD" =~ "^${git_dir}" ]]; then
     # Git branch / commit
-    local info=$(_git symbolic-ref --short HEAD || _git rev-parse --short HEAD)
+    local info=$(_git symbolic-ref --short HEAD 2>/dev/null || _git rev-parse --short HEAD)
     echo -n "%F{cyan}${info} "
 
     # Git clean / dirty
