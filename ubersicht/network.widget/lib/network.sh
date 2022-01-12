8fb361804b71d0569595d5618c2f406a1d87dbc3
Marcus Mu <chunkhang@gmail.com>
2022-01-12 12:08:49 +0800

    Remove ubersicht widgets
    - Network
    - Memory
    - Storage

--- ubersicht/network.widget/lib/network.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-function getBytes {
-    netstat -w1 > network.widget/lib/network.out & sleep 1.5; kill $!;
-}
-BYTES=$(getBytes > /dev/null);
-BYTES=$(cat network.widget/lib/network.out | grep '[0-9].*');
-BYTES=$(echo $BYTES | awk '{print $3 "^" $6}');
-echo $BYTES;
