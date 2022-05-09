716ae3f2afeefda7edfcc02a051fdc94bf1d0bce
Marcus Mu <chunkhang@gmail.com>
2022-05-09 09:44:55 +0800

    Increase font size for st

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Extended:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Iosevka Extended:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
