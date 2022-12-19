cc60f6ea30eb5db4ec442062e88e66a1ec1aa089
Marcus Mu <chunkhang@gmail.com>
2022-12-19 11:41:59 +0800

    Add rprompt

--- zsh/zsh.d/prompt.zsh
+++ zsh/zsh.d/prompt.zsh
@@ -2,24 +2,24 @@
 
 setopt PROMPT_SUBST
 
+# Do not lock git
+function _prompt::git() {
+  GIT_OPTIONAL_LOCKS=0 git "$@"
+}
+
 function _prompt() {
   # Current directory
   echo -n "%F{yellow}%~ "
 
-  # Do not lock git
-  function _git() {
-    GIT_OPTIONAL_LOCKS=0 git "$@"
-  }
-
   # Show git info if this is a git repository
-  local git_dir=$(_git rev-parse --git-dir 2>/dev/null | xargs -r realpath)
+  local git_dir=$(_prompt::git rev-parse --git-dir 2>/dev/null | xargs -r realpath)
   if [[ -n "$git_dir" && ! "$PWD" =~ "^${git_dir}.*" ]]; then
     # Git branch / commit
-    local info=$(_git symbolic-ref --short HEAD 2>/dev/null || _git rev-parse --short HEAD)
+    local info=$(_prompt::git symbolic-ref --short HEAD 2>/dev/null || _prompt::git rev-parse --short HEAD)
     echo -n "%F{cyan}${info} "
 
     # Git clean / dirty
-    if [[ -n "$(_git status --short 2>/dev/null)" ]]; then
+    if [[ -n "$(_prompt::git status --short 2>/dev/null)" ]]; then
       echo -n "%F{red}"
     else
       echo -n "%F{green}"
@@ -27,7 +27,7 @@ function _prompt() {
     echo -n "● "
 
     # Git stash
-    local count=$(_git stash list | wc -l | xargs)
+    local count=$(_prompt::git stash list | wc -l | xargs)
     if [[ "$count" != 0 ]]; then
       echo -n "%F{default}"
       for i in {1.."$count"}; do
@@ -41,4 +41,53 @@ function _prompt() {
   echo -n "%F{default}$ "
 }
 
+# Map of rprompt component to show status (1 / 0)
+typeset -A _rprompt_show
+_rprompt_show[kube]=0
+
+function context() {
+  # List rprompt components and their show statuses
+  if [[ "$#" = 0 ]]; then
+    for key in "${(@k)_rprompt_show}"; do
+      echo -n "$key"
+      if [[ "$_rprompt_show[$key]" = 1 ]]; then
+        echo " (showing)"
+      else
+        echo " (hiding)"
+      fi
+    done
+    return 0
+  fi
+
+  component="$1"
+
+  # Validate argument
+  local show="$_rprompt_show[$component]"
+  if [[ -z "$show" ]]; then
+    echo "Invalid value: ${component}"
+    return 1
+  fi
+
+  # Toggle show status for rprompt component
+  if [[ "$_rprompt_show[$component]" = 0 ]]; then
+    _rprompt_show[$component]=1
+    echo "Showing ${component}..."
+  else
+    _rprompt_show[component]=0
+    echo "Hiding ${component}..."
+  fi
+}
+
+function _rprompt() {
+  # Show current Kubernetes context and namespace
+  if [[ "$_rprompt_show[kube]" = 1 ]]; then
+    local kube_context=$(kubectl config current-context 2>/dev/null)
+    local kube_namespace=$(kubectl config view --minify --output 'jsonpath={..namespace}' 2>/dev/null)
+    if [[ -n "$kube_context" && -n "$kube_namespace" ]]; then
+      echo -n "kube[${kube_context}:${kube_namespace}]"
+    fi
+  fi
+}
+
 export PROMPT='$(_prompt)'
+export RPROMPT='$(_rprompt)'
