b24852ca3e19961f2e8d8d805fd50f019767f952
Marcus Mu <chunkhang@gmail.com>
2022-12-21 14:43:07 +0800

    Move zsh rprompt to tmux status right

--- zsh/zsh.d/prompt.zsh
+++ zsh/zsh.d/prompt.zsh
@@ -2,8 +2,6 @@
 
 setopt PROMPT_SUBST
 
-RPROMPT_CONFIG_FILE=${RPROMPT_CONFIG_FILE:-~/.rprompt.json}
-
 # Do not lock git
 function _prompt::git() {
   GIT_OPTIONAL_LOCKS=0 git "$@"
@@ -43,103 +41,4 @@ function _prompt() {
   echo -n "%F{default}$ "
 }
 
-function _rprompt::set_config_file() {
-  local value="$1"
-
-  echo "$value" > "$RPROMPT_CONFIG_FILE"
-}
-
-function _rprompt::get_config_file() {
-  # Init config file, if it does not exist
-  if [[ ! -f "$RPROMPT_CONFIG_FILE" ]]; then
-    _rprompt::set_config_file '{}'
-  fi
-
-  cat "$RPROMPT_CONFIG_FILE"
-}
-
-function _rprompt::set_config() {
-  local key="$1"
-  local value="$2"
-
-  # Set new key and value to config
-  local config=$(_rprompt::get_config_file | jq ". + { \"${key}\": ${value} }")
-  _rprompt::set_config_file "$config"
-}
-
-function _rprompt::get_config() {
-  local key="$1"
-  local default_value="$2"
-
-  local value=$(_rprompt::get_config_file | jq ".${key}")
-
-  # Return default value, if key does not exist
-  if [[ "$value" = "null" ]]; then
-    echo "$default_value"
-    return 0
-  fi
-
-  echo "$value"
-}
-
-# Cache for rprompt config
-typeset -A _rprompt_config
-
-# Set cache based on config file values
-# Also use this as list of allowed config keys, and their default values
-function _rprompt::cache_config() {
-  _rprompt_config[kube]=$(_rprompt::get_config kube 0)
-}
-_rprompt::cache_config
-
-function context() {
-  # Update cache first, to show the current config
-  _rprompt::cache_config
-
-  # List rprompt config
-  if [[ "$#" = 0 ]]; then
-    for key in "${(@k)_rprompt_config}"; do
-      echo -n "$key"
-      if [[ "$_rprompt_config[$key]" = 1 ]]; then
-        echo " (shown)"
-      else
-        echo " (hidden)"
-      fi
-    done
-    return 0
-  fi
-
-  key="$1"
-
-  # Validate key
-  local value="$_rprompt_config[$key]"
-  if [[ -z "$value" ]]; then
-    echo "Invalid value: ${key}"
-    return 1
-  fi
-
-  # Toggle rprompt config value
-  # We update the config map, but also persist it to file
-  if [[ "$_rprompt_config[$key]" = 0 ]]; then
-    echo "Showing \"${key}\" in rprompt..."
-    _rprompt_config[$key]=1
-    _rprompt::set_config "$key" 1
-  else
-    echo "Hiding \"${key}\" in rprompt..."
-    _rprompt_config[$key]=0
-    _rprompt::set_config "$key" 0
-  fi
-}
-
-function _rprompt() {
-  # Show current Kubernetes context and namespace
-  if [[ "$_rprompt_config[kube]" = 1 ]]; then
-    local kube_state=$(kubectl config get-contexts --no-headers 2>/dev/null | grep '*' | awk '{ printf "%s/%s", $2, $5 }')
-    if [[ -n "$kube_state" ]]; then
-      echo -n "kube:${kube_state}"
-    fi
-  fi
-}
-
 export PROMPT='$(_prompt)'
-export RPROMPT='$(_rprompt)'
