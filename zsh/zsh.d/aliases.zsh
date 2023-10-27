04684fde4dc41c30e7f623d5f8a59741d1ddbfd2
Marcus Mu <chunkhang@gmail.com>
2023-10-27 20:41:48 +0800

    Remove kubecolor

--- zsh/zsh.d/aliases.zsh
+++ zsh/zsh.d/aliases.zsh
@@ -64,7 +64,6 @@ alias ncdu='ncdu --color=dark --confirm-quit'
 alias newsboat='newsboat --quiet'
 alias xclip='xclip -selection clipboard'
 alias watch='watch --errexit --color'
-alias kubectl='kubecolor'
 
 # ------------------------------------------------------------------------------
 # Utilities
